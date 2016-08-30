f5baa28de941ea64cd98793cad3592ec8cec90b5
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-30 23:10:09 +0700

    manual digitalWrite, handle CS, CLK WORK

---
+++ matrix-spi-data.js
@@ -0,0 +1,58 @@
+var transpose = require('./transpose');
+
+var buildSpiData = function (txt, font){
+	//reverse bcs [o][n][a][m]=DC-5v
+	var charArr = txt.split('').reverse();
+
+	var dataArr = [];
+
+	charArr.forEach(function(char){
+		dataArr.push(font[char]);
+		// [
+		// 	[8, 9, 10, 11, 12, 13, 14, 15], //a character
+		// 	[8, 9, 10, 11, 12, 13, 14, 15], //a character
+		// 	[8, 9, 10, 11, 12, 13, 14, 15], //a character
+		// 	[8, 9, 10, 11, 12, 13, 14, 15] //a character
+		// ]
+	});
+
+	dataArr = transpose(dataArr);
+	// [
+	// 	[8, 8, 8, 8],   //all data for 4 devices in row 1 
+	// 					//=> spiData = [8, 1, 8, 1, 8, 1, 8, 1]
+	// 	[9 , 9, 9, 9],
+	// 	...
+	// 	[15, 15, 15, 15]
+	// ]
+
+	var eightSpiData = [];
+
+	dataArr.forEach(function(row, index){
+		//row = [8, 8, 8, 8]
+		var rowOrder = index + 1;
+		var spiData = [row[0], rowOrder, row[1], rowOrder, row[2], rowOrder, row[3], rowOrder];
+		//spiData [23, rowX, 45, rowX, 65, rowX, 67, rowX], 4 times <=> 4 devices
+
+		//split each val into binary "00001010"
+		var tmp = [];
+		spiData.forEach(function(rowVal){
+			var n = parseInt(rowVal, 10).toString(2);
+			n = "00000000".substr(n.length) + n;
+			var io = n.split('');
+			io.forEach(function(bin, index){
+				io[index] = Number(bin);
+			});
+			tmp = tmp.concat(io);
+		});
+
+		eightSpiData.push(tmp);
+	});
+
+	console.log(eightSpiData);
+
+	// dataArr.
+
+	return eightSpiData;
+};
+
+module.exports = buildSpiData;
--- tmp.js
+++ tmp.js
@@ -22,17 +22,37 @@ board.on("ready", function(){
 	var eightSpiData = buildSpiData('onam', matrixFont);
 
 	eightSpiData.forEach(function(spiData){
-		// var spiData =   [ 21, 7, 15, 7, 0, 7, 17, 7 ];
+		// var spiData =   [ 21, row7, 15, row7, 0, row7, 17, row7 ];        
 
 		matrix.io.digitalWrite(matrix.pins.cs, matrix.io.LOW);
 
+		console.log('old: ', !!(spiData[6-1] & (1 << (7 - 4))) | 0);
+		var nx = parseInt(spiData[6-1], 10).toString(2);
+		nx = "00000000".substr(nx.length) + nx;
+		var iox = nx.split('');
+		console.log('new:', iox[4]);
+
 		for (var j = spiData.length; j > 0; j--) {
 		// for (var j = 0; j < spiData.length; j++) {
-			matrix.board.shiftOut(matrix.pins.data, matrix.pins.clock, spiData[j - 1]);
+			var n = parseInt(spiData[j-1], 10).toString(2);
+			n = "00000000".substr(n.length) + n;
+			var io = n.split('');
+			io.forEach(function(bin, index){
+				io[index] = Number(bin);
+			});
+
+			for(var i = 0; i < 8; i++){
+				matrix.io.digitalWrite(3, 0);
+				matrix.io.digitalWrite(2, io[i]);
+				// matrix.io.digitalWrite(2, "1");
+				matrix.io.digitalWrite(3, 1);
+			}
 		}
 
 		matrix.io.digitalWrite(matrix.pins.cs, matrix.io.HIGH);
 	});
+
+	// matrix.shiftOut()
 
 	this.repl.inject({
 		matrix: matrix
