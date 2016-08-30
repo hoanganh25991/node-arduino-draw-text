dba7921e64edefefb3c0ee71665f91a768de36fd
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-30 09:58:36 +0700

    basic control on matrix

--- index.js
+++ index.js
@@ -1,109 +1,31 @@
-var temporal = require("temporal");
-var readline = require("readline");
 var five = require("johnny-five");
-// var board = new five.Board({
-//   repl: false
-// });
 var board = new five.Board();
 
-var CHARS = five.LedControl.MATRIX_CHARS;
-
 board.on("ready", function() {
-  var canWink = true;
-  var output = "";
-  var index = 0;
 
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-
-  var display = new five.Led.Matrix({
+  var matrix = new five.Led.Matrix({
     pins: {
       data: 2,
-      cs: 3,
-      clock: 4,
-    },
-    devices: 2
+      clock: 3,
+      cs: 4
+    }
   });
 
-  display.on(0);
-  display.on(1);
+  matrix.on();
 
-  function draw() {
-    if (output.length) {
+  /*
 
-      if (CHARS[output]) {
-        display.draw(0, CHARS[output]);
+  A single Led.Matrix object can control one
+  or more led matrix devices. All methods of
+  Led.Matrix objects expect the target device's
+  index as their first argument. Since this
+  might seem cumbersome when there is only
+  one matrix device, use the `device()` method
+  to create a display device bound to an index.
 
-        output = 0;
-        index = 0;
-      } else {
-        display.draw(0, output[index++]);
-      }
+  */
 
-      // Reached the end?
-      if (index === output.length) {
-        setTimeout(function() {
-          canWink = true;
-        }, 100);
-        index = 0;
-        return;
-      }
-
-      setTimeout(draw, 500);
-    }
-  }
-
-
-  function winker() {
-    var a = [0, 102, 102, 102, 0, 129, 66, 60];
-    var b = [0, 96, 96, 102, 0, 129, 66, 60];
-
-    if (canWink) {
-      display.draw(1, a);
-    }
-
-    temporal.queue([{
-      delay: 50,
-      task: function() {
-        if (canWink) {
-          display.draw(1, b);
-        }
-      }
-    }, {
-      delay: 50,
-      task: function() {
-        if (canWink) {
-          display.draw(1, a);
-        }
-      }
-    }, {
-      delay: 50,
-      task: function() {
-        if (canWink) {
-          display.draw(1, b);
-        }
-      }
-    }, {
-      delay: 50,
-      task: function() {
-        if (canWink) {
-          display.draw(1, a);
-        }
-        temporal.wait(4000, winker);
-      }
-    }]);
-  }
-
-  winker();
-
-  rl.prompt();
-  rl.on("line", function(text) {
-    output = text;
-    canWink = false;
-    rl.prompt();
-    draw();
+  this.repl.inject({
+    display: matrix.device(0)
   });
-
 });
