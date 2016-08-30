c4d06bf3ddc14702d80d651b088c4b22db040507
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-30 09:39:37 +0700

    control led-matrix

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
-    var led = new five.Led(13);
-    led.on();
 
+  var matrix = new five.Led.Matrix({
+    pins: {
+      data: 2,
+      clock: 3,
+      cs: 4
+    }
+  });
 
-    // Create an Led on pin 13 and strobe it on/off
-    // Optionall set the speed; defaults to 100ms
-    // led.strobe();
+  matrix.on();
 
-
-    this.on("exit", function() {
-        led.off();
-    });
-
+  // type `draw("shape_name")` into the repl to see the shape!  
+  this.repl.inject({
+    matrix: matrix,
+    draw: function(shape) {
+      matrix.draw(five.Led.Matrix.CHARS[shape]);
+    }
+  });
 });
