6b8c82c75cdc562a29989e68a379827ae4bb566e
Anh Le Hoang <lehoanganh25991@gmail.com>
2016-08-30 12:31:05 +0700

    char matrix draw by row > draw by column ^^

---
+++ tmp.js
@@ -0,0 +1,35 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function(){
+
+	var matrix = new five.Led.Matrix({
+		pins: {
+			data: 2,
+			clock: 3,
+			cs: 4
+		}
+	});
+
+	matrix.on();
+
+	/*
+
+	 A single Led.Matrix object can control one
+	 or more led matrix devices. All methods of
+	 Led.Matrix objects expect the target device's
+	 index as their first argument. Since this
+	 might seem cumbersome when there is only
+	 one matrix device, use the `device()` method
+	 to create a display device bound to an index.
+
+	 */
+	var display = matrix.device(0);
+	display.draw("A");
+	var a = five.LedControl.MATRIX_CHARS;
+	console.log(a['A']);
+	this.repl.inject({
+		display: matrix.device(0),
+		a: a
+	});
+});
